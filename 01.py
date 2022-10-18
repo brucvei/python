193a9c59187582096603a9e95718e9a0b4c5abf8
Bruna Caetano <bruna.caetano@acad.ufsm.br>
2022-10-18 17:53:02 -0300

    hello word

---
+++ 01.py
@@ -0,0 +1,14 @@
+print("Olá mundo")
+print(7 + 4)
+print("7" + "4")
+print("Olá ", 5)
+
+nome = input("Qual é seu nome? ")
+idade = input("Qual sua idade? ")
+peso = input("Qual seu peso? ")
+print(nome, idade, peso)
+
+n1 = input("Digite um número: ")
+n2 = input("Digite outro número: ")
+s = n1 + n2
+print()
