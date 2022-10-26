4296ef9a88ed95832e6ada2d393f8ff30fdd9ba8
Bruna Caetano <bruna.caetano@acad.ufsm.br>
2022-10-26 13:19:27 -0300

    maquina de cassino

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ symbol_value = {
 
 def check_winnings(columns, lines, bet, values):
     winnings = 0
+    winning_lines = []
     for line in range(lines):
         symbol = columns[0][line]
         for column in columns:
@@ -31,8 +32,9 @@ def check_winnings(columns, lines, bet, values):
                 break
         else:
             winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
 
-    return winnings
+    return winnings, winning_lines
 
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
@@ -107,8 +109,7 @@ def get_bet():
 
     return amount
 
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
 
     while True:
@@ -124,8 +125,20 @@ def main():
 
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
-    winnings = check_winnings(slots, lines, bet, symbol_value)
-    print(f"You won {winnings}.")
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f"Você ganhou R${winnings}.")
+    print(f"Você ganhou nas linhas: ", *winning_lines)
+    return winnings - total_bet
 
+def main():
+    balance = deposit()
+    while True:
+        print(f"Balança atual: R${balance}")
+        answer = input("Aperte enter para apostar e q para sair. ")
+        if answer == "q":
+            break
+        balance += spin(balance)
 
+    print(f"O balanço final é R${balance}")
+    
 main()
